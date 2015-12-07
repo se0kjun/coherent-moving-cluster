80b08edf0f45cc87e6ff9d76559214cf3e8a2553
SeokJun Hong <hong921122@gmail.com>
2015-12-07 23:10:01 +0900

    initial commit

---
+++ src/Cluster.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Cluster extends Obj_List {
+
+    int cluster_id;
+
+    public Cluster(int cluster_id) {this.cluster_id = cluster_id;}
+    public int getCluster_id() {return cluster_id;}
+
+}
